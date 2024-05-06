61340677b2b056c6d40df1ddeedc9321e9196461
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-05-06 13:47:04 +0300

    add swiper loop

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const embedEngine = {
     binds() {
         const swiper = new Swiper('.swiper', {
             slidesPerView: 1,
+            loop: true,
             pagination: {
                 el: '.swiper-pagination',
             },
